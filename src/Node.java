6eb4db21bbae1e264104aea83572c4b4b60f46c8
halvej66 <halvej66@uwosh.edu>
2014-04-01 13:09:16 -0500

    Node class
    Can insert Node object into a 2-D Array that will represent a Maze

---
+++ src/Node.java
@@ -0,0 +1,13 @@
+
+public class Node {
+ //boolean to check if node is a wall or not
+ public boolean isWall;
+ //indicator such as 'O' for Oliver , 'X' for wall , ' ' black space for possible path
+ public String boxIndicator;
+ //boolean to check if node is currently holding Oliver
+ public boolean isOliver;
+ //integer for X cordinate of maze
+ public int X;
+ // integer for Y cordinate of maze
+ public int Y;
+}
