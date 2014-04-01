21a2dd098f1bbe88e6ee913cf5d18d63d88525d7
halvej66 <halvej66@uwosh.edu>
2014-04-01 14:33:23 -0500

    Node class
    Set up simple class variables to indicate what position the node is in ,
    The character value which represents the standing of the node in the
    maze . I.e... if the node is a Wall 'X' or Oliver the mouse 'O'......
    Added constructors to except position X,Y on a coordinate plane and a
    character to represent the standing of the node in the maze

--- src/Node.java
+++ src/Node.java
@@ -3,11 +3,36 @@ public class Node {
  //boolean to check if node is a wall or not
  public boolean isWall;
  //indicator such as 'O' for Oliver , 'X' for wall , ' ' black space for possible path
- public String boxIndicator;
+ public char boxValue;
  //boolean to check if node is currently holding Oliver
  public boolean isOliver;
  //integer for X cordinate of maze
  public int X;
  // integer for Y cordinate of maze
  public int Y;
+ //default Constructor
+ public Node()
+ {
+	 isWall = false;
+	 boxValue = '/';
+	 isOliver = false;
+ }
+ public Node(int x , int y , char c)
+ {
+	 boxValue = c;
+	 if(boxValue == 'O')
+	 {
+		 isOliver = true;
+	 }
+	 if(boxValue =='X')
+	 {
+		 isWall = true;
+	 }
+	 X = x;
+	 Y = y;
+	 
+ }
+ 
+ 
+ 
 }
